8b59820bfb088fff241ed6736a26c2118ccd37f3
annakertesz <kerteszannanak@gmail.com>
2017-01-06 22:17:12 +0100

    sorry i just try my net connection

--- src/cigarette/TableCreator.java
+++ src/cigarette/TableCreator.java
@@ -1,7 +1,5 @@
 package cigarette;
 
-import java.sql.*;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
@@ -33,101 +31,8 @@ public class TableCreator {
 //                    "VALUES ('2016-10-20 10:45:20', 50, 20)";
 //            stmt.executeUpdate(sql);
 //
-//            sql = "INSERT INTO DIARY " +
-//                    "VALUES ('2016-10-20 23:45:20', 75, 50)";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "INSERT INTO DIARY " +
-//                    "VALUES ('2016-10-21 06:45:20', 20, 90)";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "INSERT INTO DIARY " +
-//                    "VALUES ('2016-10-21 17:45:20', 20, 70)";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "INSERT INTO DIARY " +
-//                    "VALUES ('2016-10-22 10:45:20', 50, 20)";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "INSERT INTO DIARY " +
-//                    "VALUES ('2016-10-23 06:45:20', 40, 30)";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "INSERT INTO DIARY " +
-//                    "VALUES ('2016-10-23 12:45:20', 60, 10)";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "INSERT INTO DIARY " +
-//                    "VALUES ('2016-10-23 21:45:20', 65, 12)";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "INSERT INTO DIARY " +
-//                    "VALUES ('2016-10-24 14:45:20', 70, 7)";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "INSERT INTO DIARY " +
-//                    "VALUES ('2016-10-24 23:45:20', 30, 50)";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "INSERT INTO DIARY " +
-//                    "VALUES ('2016-10-25 14:45:20', 50, 20)";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "INSERT INTO CIGARETTE " +
-//                    "VALUES ('2016-10-20 14:45:20')";
-//            stmt.executeUpdate(sql);
-//
-//            sql = "INSERT INTO CIGARETTE " +
-//                    "VALUES ('2016-10-20 23:45:20')";
-//            stmt.executeUpdate(sql);
-//
-//
-//            sql = "INSERT INTO CIGARETTE " +
-//                    "VALUES ('2016-10-21 08:45:20')";
-//            stmt.executeUpdate(sql);
-//
-//
-//            sql = "INSERT INTO CIGARETTE " +
-//                    "VALUES ('2016-10-21 14:45:20')";
-//            stmt.executeUpdate(sql);
-//
-//
-//            sql = "INSERT INTO CIGARETTE " +
-//                    "VALUES ('2016-10-21 18:45:20')";
-//            stmt.executeUpdate(sql);
-//
-//
-//            sql = "INSERT INTO CIGARETTE " +
-//                    "VALUES ('2016-10-21 23:45:20')";
-//            stmt.executeUpdate(sql);
-//
-//
-//            sql = "INSERT INTO CIGARETTE " +
-//                    "VALUES ('2016-10-22 13:45:20')";
-//            stmt.executeUpdate(sql);
-//
-//
-//            sql = "INSERT INTO CIGARETTE " +
-//                    "VALUES ('2016-10-22 23:45:20')";
-//            stmt.executeUpdate(sql);
-//
-//
-//            sql = "INSERT INTO CIGARETTE " +
-//                    "VALUES ('2016-10-23 11:45:20')";
-//            stmt.executeUpdate(sql);
-//
-//
-//            sql = "INSERT INTO CIGARETTE " +
-//                    "VALUES ('2016-10-24 18:45:20')";
-//            stmt.executeUpdate(sql);
-//
-//
-//            sql = "INSERT INTO CIGARETTE " +
-//                    "VALUES ('2016-10-25 14:45:20')";
-//            stmt.executeUpdate(sql);
 
-
-
+//try git connectrion
 
             stmt.close();
             c.close();
